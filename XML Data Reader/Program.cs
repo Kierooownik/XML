05e6a69aa69809683984f2e50044a0a0f5c62ee9
Kierooownik <chopinek93@gmail.com>
2023-08-15 17:53:34 +0200

    Initial version

---
+++ XML Data Reader/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_Data_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path to the XML file:");
+            string xmlFilePath = Console.ReadLine();
+
+            ProcessXmlFile(xmlFilePath);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); // Czekaj na wciśnięcie dowolnego klawisza
+        }
+
+        static void ProcessXmlFile(string xmlFilePath)
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlFilePath);
+
+                XmlNodeList arrayOfParameterNodes = xmlDoc.SelectNodes("//chanelParameter/ArrayOfParameter");
+
+                foreach (XmlNode arrayOfParameterNode in arrayOfParameterNodes)
+                {
+                    XmlAttribute nameAttribute = arrayOfParameterNode.Attributes["name"];
+                    if(nameAttribute !=null)
+                    {
+                        string arrayOfParameterName = nameAttribute.Value;
+                        Console.WriteLine();
+                        Console.WriteLine(arrayOfParameterName);
+                    }
+                    
+
+                    XmlNodeList parameterNodes = arrayOfParameterNode.SelectNodes("Parameter");
+
+                    foreach (XmlNode parameterNode in parameterNodes)
+                    {
+                        string name = parameterNode.SelectSingleNode("name").InnerText;
+                        string value = parameterNode.SelectSingleNode("value").InnerText;
+                        string index = parameterNode.SelectSingleNode("index").InnerText;
+
+                        Console.WriteLine($"{index} {name} = {value}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ XML Data Reader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("XML Data Reader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XML Data Reader")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("e10184b7-c990-4ccb-8d4c-f56af0702f65")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

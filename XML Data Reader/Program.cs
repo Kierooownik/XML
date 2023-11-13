2598b6e30540c07bc6ace56aecbd8dc1961cdc4b
Kierooownik <chopinek93@gmail.com>
2023-11-13 20:35:35 +0100

    Output path modification
    Poprawiony format ścieżki pliku wynikowego. Dodana data i numer materiałowy dla porównywanych plików

--- XML Data Reader/Program.cs
+++ XML Data Reader/Program.cs
@@ -25,9 +25,6 @@ namespace XML_Data_Reader
             string folderPath = Console.ReadLine();
             string[] xmlFiles = Directory.GetFiles(folderPath, "*.xml");
 
-            //Utworzenie ścieżki wyjściowej dla pliku Excel
-            string outputPath = Path.Combine(folderPath, "output.xlsx");
-            
             //Wybór typu wyświetlania danych
             Console.WriteLine($"Aby otrzymać pełne dane wpisz 'full', aby otrzymać tylko parametry z różniącymi się wartościami wpisz 'diff'");
             string displayType = Console.ReadLine();
@@ -35,59 +32,78 @@ namespace XML_Data_Reader
             //Przetworzenie pliku XML referencyjnego i przechowanie danych w słowniku
             Dictionary<int, ParameterInfo> referenceParameters = ProcessXmlFile(referencePath);
 
-            //Inicjalizacja pakietu Excel
-            using (var package = new ExcelPackage())
+            //Utworzenie ścieżki wyjściowej dla pliku Excel
+            string currentDateTime = DateTime.Now.ToString("yyyyMMdd_HHmm");
+            string materialNumber = referenceParameters[65501].Value;
+            string outputPath = Path.Combine(folderPath, $"SAP{materialNumber}_XML_Compare_{currentDateTime}.xlsx");
+            //string outputPathSAP = $"{outputPath}_SAP{materialNumber}.xlsx";
+
+            try
             {
-                var worksheet = package.Workbook.Worksheets.Add("Dane");
-
-                //Ustawienie indeksu wiersza referencyjnego
-                int rowReferenceIndex = 2;
-                worksheet.Cells[1, 3].Value = referenceParameters[31318].Value;
-                foreach (var parameter in referenceParameters)
+                //Inicjalizacja pakietu Excel
+                using (var package = new ExcelPackage())
                 {
-                    //Wypełnienie arkusza danymi z pliku referencyjnego
-                    worksheet.Cells[rowReferenceIndex, 1].Value = parameter.Key;
-                    worksheet.Cells[rowReferenceIndex, 2].Value = parameter.Value.Name;
-                    worksheet.Cells[rowReferenceIndex, 3].Value = parameter.Value.Value;
-                    rowReferenceIndex++;
-                }
+                    var worksheet = package.Workbook.Worksheets.Add("Dane");
 
-                //Analiza danych zgodnie z wybranym typem wyświetlania
-                if (displayType == "full")
-                {
-                    ProcessXmlToWorksheet(worksheet, xmlFiles, referencePath, referenceParameters);
-                    
-                }
-                else if (displayType == "diff")
-                {
-                    ProcessXmlToWorksheet(worksheet, xmlFiles, referencePath, referenceParameters);
-
-                    //Usuwanie wierszy z takimi samymi ustawieniami we wszystkich plikach
-                    for(int row = worksheet.Dimension.End.Row; row >= 2; row--)
+                    //Ustawienie indeksu wiersza referencyjnego
+                    int rowReferenceIndex = 2;
+                    worksheet.Cells[1, 3].Value = referenceParameters[31318].Value;
+                    foreach (var parameter in referenceParameters)
                     {
-                        if(AllCellsInRowEqual(worksheet, row))
-                        {
-                            worksheet.DeleteRow(row);
-                            //Console.WriteLine(AllCellsInRowEqual(worksheet, row));
-                        }
+                        //Wypełnienie arkusza danymi z pliku referencyjnego
+                        worksheet.Cells[rowReferenceIndex, 1].Value = parameter.Key;
+                        worksheet.Cells[rowReferenceIndex, 2].Value = parameter.Value.Name;
+                        worksheet.Cells[rowReferenceIndex, 3].Value = parameter.Value.Value;
+                        rowReferenceIndex++;
                     }
 
-                }
-                else
-                {
-                    Console.WriteLine("Wrong input");
-                    return;
-                }
+                    //Analiza danych zgodnie z wybranym typem wyświetlania
+                    if (displayType == "full")
+                    {
+                        ProcessXmlToWorksheet(worksheet, xmlFiles, referencePath, referenceParameters);
 
-                var fileInfo = new FileInfo(outputPath);
-                package.SaveAs(fileInfo);
-                
+                    }
+                    else if (displayType == "diff")
+                    {
+                        ProcessXmlToWorksheet(worksheet, xmlFiles, referencePath, referenceParameters);
+
+                        //Usuwanie wierszy z takimi samymi ustawieniami we wszystkich plikach
+                        for (int row = worksheet.Dimension.End.Row; row >= 2; row--)
+                        {
+                            if (AllCellsInRowEqual(worksheet, row))
+                            {
+                                worksheet.DeleteRow(row);
+                                //Console.WriteLine(AllCellsInRowEqual(worksheet, row));
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input");
+                        return;
+                    }
+
+
+                    var fileInfo = new FileInfo(outputPath);
+                    package.SaveAs(fileInfo);
+
+                }
+                //Informacje o zakończeniu eksportu danych do pliku Excel
+                Console.WriteLine("Export to .xls finished");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+                System.Diagnostics.Process.Start(outputPath);
             }
-            //Informacje o zakończeniu eksportu danych do pliku Excel
-            Console.WriteLine("Export to .xls finished");
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
-            System.Diagnostics.Process.Start(outputPath);
+            catch (UnauthorizedAccessException unauthorizedEx)
+            {
+                Console.WriteLine($"Unauthorized access exception: {unauthorizedEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            
 
         }
         //Klasa zawierająca informacje o parametrze
